8887bcd64c7255a00aab404be5af82287d8a65be
adriand0513 <97262258+adriand0513@users.noreply.github.com>
2023-10-15 04:45:04 -0400

    password.py

---
+++ password.py
@@ -0,0 +1,15 @@
+import random
+
+lower = 'abcdefghijklmnopqrstuvwxyz'
+upper = lower.upper()
+symbols = '!@#$%^&*()_+-=[]{};:,./<>?'
+numbers = '1234567890'
+all = lower + upper + symbols + numbers
+
+length = 10
+
+password = ''
+for _ in range(length):
+    password = ''.join([password, random.choice(all)])
+
+print(password)
